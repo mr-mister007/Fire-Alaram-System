8dfa81c0559be5c991f90f87c14f4caa447a403c
Groot <48949823+mr-mister007@users.noreply.github.com>
2023-02-16 19:52:31 +0530

    Add files via upload

---
+++ Fire Alaram System.py
@@ -0,0 +1,52 @@
+from twilio.rest import Client
+from pushbullet import Pushbullet
+import sys
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN) 
+import smtplib
+pb = Pushbullet("********")
+
+def send_email1(user, pwd, recipient, subject, body):
+    gmail_user = '*****@gmail.com'
+    gmail_pwd = '*******'
+    FROM = user
+    TO = 'firealertpi@gmail.com'
+    SUBJECT = 'Fire'
+    TEXT = body
+
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        print "trying to send email"
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+	
+    except:
+        print "failed to send mail"
+
+def send1():
+    	send_email1('********@gmail.com', '******', 'firealertpi@gmail.com', 'Fire!!!', 'Building On Fire!!!')
+    	
+
+	account_sid = '*****'
+	auth_token = '*****'
+	client = Client(account_sid, auth_token)
+
+	call = client.calls.create(
+                        	url='http://demo.twilio.com/docs/voice.xml',
+                        	to='+918330035029',
+                        	from_='+12052369265'
+                    	)
+
+	
+while 1:
+    if not GPIO.input(11):
+	send1()   
+        sys.exit()
